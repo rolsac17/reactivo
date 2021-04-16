5fdf717ac3c136d317625e52e03a49d1b41881d2
rolsac17 <rolandodepaz17@gmail.com>
2021-04-16 17:21:36 -0600

    Iniciando la app

---
+++ 03-counter-app/src/PrimeraApp.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+
+//hay componentes basados en clases y en funciones
+//trabajaremos con funcional components antes se llamaban steylas funcional component.
+
+const PrimeraApp = () =>{
+    return(
+        <Fragment>
+        <h1>Hola mundo</h1>
+        <p>Mi primera Aplicacion</p>
+        </Fragment>
+    )
+}
+
+export default PrimeraApp;
