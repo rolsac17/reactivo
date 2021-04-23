d19e9e449df1f74f17d9c306f2812b3216e852d6
rolsac17 <rolandodepaz17@gmail.com>
2021-04-23 17:07:15 -0600

    proptypes

---
+++ 03-counter-app/src/CounterApp.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CounterApp = () =>{
+    return(
+        <h1>ejemplo</h1>
+    )
+}
+
+export default CounterApp;
--- 03-counter-app/src/PrimeraApp.js
+++ 03-counter-app/src/PrimeraApp.js
@@ -1,17 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 //hay componentes basados en clases y en funciones
 //trabajaremos con funcional components antes se llamaban steylas funcional component.
 
 // para que no aparesca el div podemos importa Fragment o usar la abreviatura <></>
-const PrimeraApp = ( props ) =>{
+const PrimeraApp = ( {saludo, subtitulo} ) =>{
+// se desestructura y asi no se agrega la palabra props
 
+// if(!saludo){ asi se validaria de una mala forma que si venga la prop
+//     throw new Error('El saludo es necesario');
+// }
     return(
         <>
-        <h1>{props.saludo}</h1>
-        <p>Mi primera Aplicacion</p>
+        <h1>{saludo}</h1>
+        <p>{subtitulo}</p>
         </>
     )
+}
+
+//para que la prop sea obligatoria se agrega el isRequired despues del tipo de la prop
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired
+}
+
+//Para colocar por default props aparte de definirlos en los parentesis del componente
+PrimeraApp.defaultProps ={
+    subtitulo: 'Esta es una descripcion de inquebrantable'
 }
 
 export default PrimeraApp;
