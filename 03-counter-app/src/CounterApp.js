85eb829c9dc23303147400e30c51da9c2c49a002
rolsac17 <rolandodepaz17@gmail.com>
2021-04-29 10:28:58 -0600

    iniciando seccion pruebas

--- 03-counter-app/src/CounterApp.js
+++ 03-counter-app/src/CounterApp.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-const CounterApp = ({ valor }) =>{
+const CounterApp = ({ valor= 10 }) =>{
+
+     // const [ nombre, setNombre ] = useState('Goku');  
+    // console.log(nombre, setNombre);  
+    const [ counter, setCounter ] = useState( valor );
+
+    //handleAdd
+    const handleAss = () =>{
+       // setCounter(counter +1); primera manera de manejar
+       setCounter((c)=> c + 1);
+    }
+
+    //handleSubstract
+    const handleSubstract = () => {
+        setCounter(c => c - 1);
+    }
+
+    //handleReset
+    const handleReset = () => {
+        setCounter( valor);
+    }
+
     return(
         <>
         <h1>CounterApp</h1>
-        <h2>{valor}</h2>
+        <h2>{counter}</h2>
+
+        <button onClick={handleAss}>+ 1</button>
+        <button onClick={handleReset}>Reset</button>
+        <button onClick={handleSubstract}>- 1</button>
         </>
     )
 }
